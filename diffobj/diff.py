01832aa0b48a661a3d3625e2a80446e0f096b7e5
Adam Brandizzi <adam.brandizzi@liferay.com>
2015-02-05 10:04:27 -0300

    Refactoring

--- diffobj/diff.py
+++ diffobj/diff.py
@@ -22,21 +22,22 @@ def diff(old, new):
 def patch(base, diff):
     for operation in diff:
         operator, attr = operation[:2]
-        if operator in ('create', 'update'):
+
+        if operator == 'create':
             value = operation[2]
-            if (operator == 'create') and hasattr(base, attr):
+            if hasattr(base, attr):
                 raise Conflict('Attribute {0} already exists.'.format(attr))
-            elif operator == 'update':
-                curr_value = getattr(base, attr)
-                old_value = operation[2]
-                if old_value != curr_value:
-                    raise Conflict(
-                        'Attribute {0} is {1} - expected {2}.'.format(
-                            attr, curr_value, old_value
-                        )
-                    )
-                value = operation[3]
             setattr(base, attr, value)
+        elif operator == 'update':
+            curr_value = getattr(base, attr)
+            old_value, new_value = operation[2:4]
+            if old_value != curr_value:
+                raise Conflict(
+                    'Attribute {0} is {1} - expected {2}.'.format(
+                        attr, curr_value, old_value
+                    )
+                )
+            setattr(base, attr, new_value)
         elif operator == 'drop':
             delattr(base, attr)
 
